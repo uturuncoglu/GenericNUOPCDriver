efcf9be41a96c832a7b543d9292b9ce1dc402d68
Ufuk Turuncoglu <turuncu@ucar.edu>
2022-07-31 00:24:58 -0600

    fix seperator issue

--- scripts/gen_config.py
+++ scripts/gen_config.py
@@ -104,7 +104,11 @@ def gen_config(_dict, ifile):
 
                     # write config file in specified format
                     if 'nuopc' in k2:
-                        pg.write_nuopc(ofile, append=append[k3])
+                        sep = ':'
+                        if 'seperator' in v2:
+                            if v2['seperator'].lower() == 'eq':
+                                sep = ' ='
+                        pg.write_nuopc(ofile, append=append[k3], seperator=sep)
                     elif 'nml' in k2:
                         pg.write_nml(ofile, append=append[k3])
                     else:
